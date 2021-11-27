c58f889cad6a211cac7f49c8ea1b31593dd2c464
RiteshSulakhe1 <riteshsulakhe123@gmail.com>
2021-11-27 19:57:41 +0530

    added docs route

--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -28,12 +28,15 @@ const devRoutes = [
 defaultRoutes.forEach((route) => {
   router.use(route.path, route.route);
 });
+devRoutes.forEach((route) => {
+  router.use(route.path, route.route);
+});
 
 /* istanbul ignore next */
-if (config.env === 'development') {
-  devRoutes.forEach((route) => {
-    router.use(route.path, route.route);
-  });
-}
+// if (config.env === 'development') {
+//   devRoutes.forEach((route) => {
+//     router.use(route.path, route.route);
+//   });
+// }
 
 module.exports = router;
