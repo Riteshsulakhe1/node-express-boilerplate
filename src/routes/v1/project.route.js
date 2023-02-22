2d31718c573bdd1cc526dea3bbac81b347e92635
RiteshSulakhe1 <riteshsulakhe123@gmail.com>
2023-02-22 17:25:17 +0530

    Modified project API docs

--- src/routes/v1/project.route.js
+++ src/routes/v1/project.route.js
@@ -26,16 +26,16 @@ module.exports = router;
  * @swagger
  * 
  * /project:
- *  post:
- *      summary: Create a project
- *      description: User (Organization admin) can create project.
- *      tags: [Projects]
- *      security:
- *          - bearerAuth: []
- *      requestBody:
- *          required: true
- *          content:
- *              application/json:
+ *      post:
+ *          summary: Create a project
+ *          description: User (Organization admin) can create project.
+ *          tags: [Projects]
+ *          security:
+ *              - bearerAuth: []
+ *          requestBody:
+ *              required: true
+ *              content:
+ *                  application/json:
  *                  schema:
  *                      type: object
  *                      required:
@@ -49,11 +49,11 @@ module.exports = router;
  *                      example:
  *                          name: Task Manager
  *                          key: TM
- *      responses:
- *          "201":
- *              description: Created
- *              content:
- *                  application/json:
+ *          responses:
+ *              "201":
+*                   description: Created
+ *                  content:
+ *                      application/json:
  *                      schema:
  *                          type: object
  *                          properties:
@@ -61,59 +61,59 @@ module.exports = router;
  *                                  $ref: '#/components/schemas/Project'
  *                              message: Project created successfully
  *
- *          "422":
- *              description: User don't have access to create project (Only org admin have access)
+ *              "422":
+ *                  description: User don't have access to create project (Only org admin have access)
  *
- *           "409":
- *              description: Project key is already used. Please change the key
+ *              "409":
+ *                  description: Project key is already used. Please change the key
  */
 
 /**
  * @swagger
  * /project/{projectId}:
- *  put:
- *      summary: Update project settings
- *      description: User can update project name, key, epics, labels
- *      tags: [Projects]
- *      security:
- *          - bearerAuth: []
- *      requestBody:
- *          required: true
- *          content:
- *              application/json
- *              schema:
- *                  required:
- *                      -name
- *                      -key
- *                  properties:
- *                      name:
- *                          type: string
- *                      key:
- *                          type: string
- *                      epics:
- *                          type: array
- *                      labels:
- *                          type: array
- *                  example:
- *                      name: Project 1
- *                      key: p1
- *                      epics: [epic1]
- *                      labels: [label1]
- *
- *
- *      responses:
- *          "200":
- *              description: OK
+ *      put:
+ *          summary: Update project settings
+ *          description: User can update project name, key, epics, labels
+ *          tags: [Projects]
+ *          security:
+ *              - bearerAuth: []
+ *          requestBody:
+ *              required: true
  *              content:
- *                  application/json:
+ *                  application/json
  *                  schema:
- *                      type: object
+ *                      required:
+ *                          -name
+ *                          -key
  *                      properties:
- *                          project:
- *                              $ref: '#/components/schemas/Project'
- *                          message: Project upated successfully
+ *                          name:
+ *                              type: string
+ *                          key:
+ *                              type: string
+ *                          epics:
+ *                              type: array
+ *                          labels:
+ *                              type: array
+ *                      example:
+ *                          name: Project 1
+ *                          key: p1
+ *                          epics: [epic1]
+ *                          labels: [label1]
  *
- *          "422":
- *              description: User don't have access to update project (Only org admin have access)
+ *
+ *          responses:
+ *              "200":
+ *                  description: OK
+ *                  content:
+ *                      application/json:
+ *                      schema:
+ *                          type: object
+ *                          properties:
+ *                              project:
+ *                                  $ref: '#/components/schemas/Project'
+ *                              message: Project upated successfully
+ *
+ *              "422":
+ *                  description: User don't have access to update project (Only org admin have access)
  *
  */
